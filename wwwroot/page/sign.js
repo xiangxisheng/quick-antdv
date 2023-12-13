64580c4232defbbb566c2d297223a4500617e95d
XiangXisheng <xiangxisheng@gmail.com>
2023-12-13 10:35:08 +0800

    登录页面

--- wwwroot/page/sign.js
+++ wwwroot/page/sign.js
@@ -1,9 +1,40 @@
+const { reactive } = Vue;
 const { useRoute } = VueRouter;
+const { Form, FormItem, Input, InputPassword, Checkbox, Button } = antd;
 export default async () => ({
   template: await (await fetch('./page/sign.htm')).text(),
+  components: {
+    AForm: Form,
+    AFormItem: FormItem,
+    AInput: Input,
+    AInputPassword: InputPassword,
+    ACheckbox: Checkbox,
+    AButton: Button,
+  },
   setup() {
+
+    const formState = reactive({
+      username: '',
+      password: '',
+      remember: false,
+    });
+    const onFinish = (values) => {
+      console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
+
     const route = useRoute();
-    const title = route.name;
-    return { title };
+    const title = route.name === '/sign-up' ? 'Register' : 'Login';
+
+    return {
+      route,
+      title,
+      formState,
+      onFinish,
+      onFinishFailed,
+    };
   },
 })
