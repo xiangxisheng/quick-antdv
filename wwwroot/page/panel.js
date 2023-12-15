21e806ab120990411fbf480391f1c4afc7ac6805
XiangXisheng <xiangxisheng@gmail.com>
2023-12-15 15:58:42 +0800

    1

--- wwwroot/page/panel.js
+++ wwwroot/page/panel.js
@@ -1,5 +1,5 @@
 import { routes_filter } from 'firadio';
-const { ref, watch } = Vue;
+const { ref, reactive, watch, h } = Vue;
 const { useRouter, useRoute } = VueRouter;
 const { Layout, LayoutContent, LayoutFooter, LayoutSider, Menu, Breadcrumb, BreadcrumbItem } = antd;
 export default async (oTopRoute) => ({
@@ -19,12 +19,20 @@ export default async (oTopRoute) => ({
     const items = ref([]);
     const openKeys = ref(['/console/data']);
     const selectedKeys = ref([]);
-    const collapsed = ref(false);
+    const menuState = reactive({
+      collapsed: false,
+    });
     selectedKeys.value = [route.name];
     (async () => {
       items.value = (await routes_filter(oTopRoute, async (sParent, mRoute) => {
         const mRet = {};
         mRet.key = `${sParent}/${mRoute.name}`;
+        mRet.icon = () => h({
+          template: '<i class="bx bx-radio-circle" style="font-size: 16px; vertical-align: -2px; margin-right: 4px;"></i>',
+          setup() {
+            return {};
+          }
+        });
         mRet.label = mRoute.label;
         return mRet;
       })).children;
@@ -44,7 +52,7 @@ export default async (oTopRoute) => ({
       items,
       openKeys,
       selectedKeys,
-      collapsed,
+      menuState,
       handleClick,
     };
   },
