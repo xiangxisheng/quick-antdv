9039296d7f12609694c7fb636e3630956aaff41b
firadio <git@firadio.com>
2024-01-30 14:55:50 +0700

    1

--- php-serve-ipv6.bat
+++ php-serve-ipv6.bat
@@ -2,5 +2,5 @@
 cd /d %~dp0
 for %%i in (%0) do (set "name=%%~ni")
 title %name%
-php -c %~dp0php/php.ini -S 0.0.0.0:8000 -t %~dp0wwwroot
+php -c %~dp0php/php.ini -S [::]:8000 -t %~dp0wwwroot
 PAUSE
--- php/class.php
+++ php/class.php
@@ -4,5 +4,6 @@ namespace QuickPHP;
 
 define('CLASS_DIR', __DIR__ . '/class');
 spl_autoload_register(function ($class) {
+	$class = str_replace("\\", '/', $class);
 	require_once CLASS_DIR . '/' . $class . '.php';
 });
